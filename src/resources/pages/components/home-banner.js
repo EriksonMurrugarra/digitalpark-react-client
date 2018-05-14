1a02c4b3f3f67b5ad36b1de0660cfeb4cf1744c9
Erikson Murrugarra <erikson.murrugarra@gmail.com>
2018-05-14 00:43:14 -0500

    changing banner text

--- src/resources/pages/components/home-banner.js
+++ src/resources/pages/components/home-banner.js
@@ -10,7 +10,7 @@ const HomeBanner = ({ authenticated }) => (
   <section className="app-banner" style={bannerStyle}>
     <aside>
       <div>
-        <h1 className="app-banner-title">Experiencias vividas sobre Desarrollo de Aplicaciones, Cloud Computing y DevOps.</h1>
+        <h1 className="app-banner-title">Compartiendo experiencias sobre Desarrollo de Aplicaciones, Cloud Computing y DevOps.</h1>
         {
           !authenticated &&
           <button className="btn-green btn-join">Unete Ahora!</button>
